acaa786f4c990d6a9329574acb183c483df4df74
Giovanni <gfeltrin25@gmail.com>
2026-01-16 19:02:42 +0100

    Update favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
     import './layout.css';
-    import favicon from '$lib/assets/favicon.svg';
     import 'xp.css/dist/XP.css';
 
     let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head><link rel="icon" href="/favicon.ico" /></svelte:head>
 <div style="height: 100dvh; width: 100dvw;">
     {@render children()}
 </div>
